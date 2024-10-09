b8f9dbaafe0e47168dbc0a2054d5a007596fa8e7
xerdin442 <mudianthonio27@gmail.com>
2024-10-09 14:50:22 +0100

    modified handling of request in webhook

--- src/shared/util/webhook.ts
+++ src/shared/util/webhook.ts
@@ -12,21 +12,27 @@ import { sendEmail, ticketRefundMail } from './mail';
 const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY as string
 
 async function pasytackCallback(req: Request, res: Response) {
-  console.log('Webhook logic initiated')
-  
   const hash = crypto.createHmac('sha512', PAYSTACK_SECRET_KEY)
     .update(JSON.stringify(req.body)).digest('hex')
 
   if (hash === req.headers['x-paystack-signature']) {
-    const { event, data } = req.body
-    const { userId, eventId, discount, tier, quantity } = data.recipient.metadata
+    res.sendStatus(200) // Send a 200 OK response to Paystack server
+    
+    const { event } = req.body
+    let data: Record<string, any>;
+    
+    if (event.includes('transfer')) {
+      data = req.body.data.recipient
+    } else if (event.includes('charge')) {
+      data = req.body.data
+    }
 
+    console.log(data)
+    const { userId, eventId, discount, tier, quantity } = data.metadata
     const user = await User.findById(userId)
 
     // Listen for status of transfers for ticket refunds and revenue splits
     if (event === 'transfer.success') {
-      res.sendStatus(200) // Send a 200 OK response to Paystack server
-
       if (data.reason === 'Ticket Refund') {
         // Remove the attendee as a transfer recipient after the refund is complete
         await deleteTransferRecipient(data.recipient.recipient_code)
@@ -40,15 +46,11 @@ async function pasytackCallback(req: Request, res: Response) {
       console.log(`${data.reason}: Transfer to ${user.fullname} was successful!`)
       return true;
     } else if (event === 'transfer.failed') {
-      res.sendStatus(200) // Send a 200 OK response to Paystack server
-
       console.log(`${data.reason}: Transfer to ${user.fullname} failed.`)
       // ***Retry transfer
 
       return true;
     } else if (event === 'transfer.reversed') {
-      res.sendStatus(200) // Send a 200 OK response to Paystack server
-
       console.log(`${data.reason}: Transfer to ${user.fullname} has been reversed.`)
       // ***Retry transfer
 
@@ -57,18 +59,19 @@ async function pasytackCallback(req: Request, res: Response) {
 
     // Listen for status of transactions for ticket purchases
     if (event === 'charge.success') {
-      res.sendStatus(200) // Send a 200 OK response to Paystack server
-      
-      await completeTicketPurchase(data.recipient.metadata)
-
-      // Emit a success message to the user's WebSocket connection
+      // Emit a success message to the frontend WebSocket connection
       if (clients[userId]) {
         clients[userId].send(JSON.stringify({ status: 'success', message: 'Payment successful!' }));
       }
 
+      await completeTicketPurchase(data.recipient.metadata)
+
       return true;
     } else if (event === 'charge.failed') {
-      res.sendStatus(200) // Send a 200 OK response to Paystack server
+      // Emit a failure message to the frontend WebSocket connection
+      if (clients[userId]) {
+        clients[userId].send(JSON.stringify({ status: 'failed', message: 'Payment failed!' }));
+      }
 
       // Update number of tickets after failed ticket purchase
       const event = await Event.findById(eventId)
@@ -77,11 +80,6 @@ async function pasytackCallback(req: Request, res: Response) {
       if (discount) { ticket.discount.numberOfTickets += quantity }
       ticket.totalNumber += quantity
       await event.save()
-
-      // Emit a failure message to the user's WebSocket connection
-      if (clients[userId]) {
-        clients[userId].send(JSON.stringify({ status: 'failed', message: 'Payment failed!' }));
-      }
 
       return true;
     }
