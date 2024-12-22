b4392d79981ee07b8c660ff5626c2d13ec80da0e
xerdin442 <mudianthonio27@gmail.com>
2024-12-22 17:49:04 +0100

    moving to nestjs

--- src/tickets/ticket.router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import * as Ticket from './ticket.controller';
-import { isEventOwner, isLoggedIn } from '../shared/middlewares/authorization';
-
-export default (router: express.Router) => {
-  router.post('/events/:eventId/tickets/purchase', isLoggedIn, Ticket.purchaseTicket)
-  router.post('/events/:eventId/tickets/validate', isLoggedIn, isEventOwner, Ticket.validateTicket)
-}
