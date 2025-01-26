e0dcdd25020e21f83fd2c52c667ae95385ee0735
xerdin442 <mudianthonio27@gmail.com>
2025-01-26 01:13:18 +0100

    fixed double metric creatio

--- src/metrics/metrics.service.ts
+++ src/metrics/metrics.service.ts
@@ -3,29 +3,25 @@ import { Counter, Gauge, Registry } from 'prom-client';
 
 @Injectable()
 export class MetricsService {
-  public readonly twoFactorAuthMetric: Gauge<string>;
-  public readonly unsuccessfulTransfersCounter: Counter<string>;
-  public readonly transactionRefundCounter: Counter<string>;
+  public readonly twoFactorAuthMetric: Gauge<string> = new Gauge({
+    name: 'two_fa_enabled_users',
+    help: 'Total number of users that enabled 2FA',
+    registers: [this.registry]
+  });
 
-  constructor(private readonly registry: Registry) {
-    this.twoFactorAuthMetric = new Gauge({
-      name: 'two_fa_enabled_users',
-      help: 'Total number of users that enabled 2FA',
-      registers: [this.registry]
-    });
+  public readonly unsuccessfulTransfersCounter: Counter<string> = new Counter({
+    name: 'transaction_refunds',
+    help: 'Total number of transaction refunds',
+    registers: [this.registry]
+  });
 
-    this.transactionRefundCounter = new Counter({
-      name: 'transaction_refunds',
-      help: 'Total number of transaction refunds',
-      registers: [this.registry]
-    })
+  public readonly transactionRefundCounter: Counter<string> = new Counter({
+    name: 'unsuccessful_transfers',
+    help: 'Total number of unsuccessful transfers',
+    registers: [this.registry]
+  });
 
-    this.unsuccessfulTransfersCounter = new Counter({
-      name: 'unsuccessful_transfers',
-      help: 'Total number of unsuccessful transfers',
-      registers: [this.registry]
-    })
-  }
+  constructor(private readonly registry: Registry) { };
 
   async getMetrics(): Promise<Record<string, any>> {
     return await this.registry.getMetricsAsJSON();
